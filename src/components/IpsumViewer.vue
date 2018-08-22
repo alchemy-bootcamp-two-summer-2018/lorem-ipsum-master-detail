d073a62616922f8a96ff8e92075af43041e66a34
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-08-22 15:46:48 -0700

    form hooked up

--- src/components/IpsumViewer.vue
+++ src/components/IpsumViewer.vue
@@ -1,13 +1,27 @@
 <template>
     <div> 
-        <section>
-          <IpsumDetail :ipsum="ipsum"/>
-        </section>
+      <section v-if="ipsum">
+        <p v-if="!editing">
+          <button @click="editing = true">Edit</button>
+        </p>
+       <IpsumForm v-if="editing"
+        :ipsum="ipsum"
+        :onUpdate="handleUpdate"
+        :onCancel="handleEndEdit"
+        />
+        <IpsumDetail v-else :ipsum="ipsum"/>
+      </section>
+
+      <section v-else>
+          <p> Choose an Ipsum </p>
+      </section>
+
     </div>
 </template>
 
 <script>
 import IpsumDetail from './IpsumDetail.vue';
+import IpsumForm from './IpsumForm.vue';
 export default {
   props: {
     ipsum: Object,
@@ -19,7 +33,8 @@ export default {
     };
   },
   components: {
-    IpsumDetail
+    IpsumDetail,
+    IpsumForm
   },
   methods: {
     handleEndEdit() {
