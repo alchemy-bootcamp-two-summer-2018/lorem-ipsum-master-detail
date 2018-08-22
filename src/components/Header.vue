dcf4c444c0f19c8533eb183667a9b93fcc7758d4
Jack <ajackofsometrades@gmail.com>
2018-08-22 13:41:41 -0700

    start of header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <header>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Ipsum Warehouse</h1>
+    <p>Need an ipsum? We got it.</p>
+  </header>
 </template>
 
 <script>
@@ -9,5 +13,13 @@ export default {
 </script>
 
 <style>
+
+img {
+  width: 150px;
+}
+
+h1 {
+  font-size: 48px;
+}
 
 </style>
