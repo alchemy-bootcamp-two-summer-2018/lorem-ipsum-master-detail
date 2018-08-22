01f4e7167bf7763347b433614b2c88ee9c0b5053
Sophie Graham <sophiergraham1@gmail.com>
2018-08-22 14:47:19 -0700

    each item has selected box

--- src/components/IpsumItem.vue
+++ src/components/IpsumItem.vue
@@ -20,6 +20,14 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+  li {
+    list-style: none;
+    text-align: center;
+    border: 1px solid #aaa;
+  }
+  .selected {
+    background: lightsteelblue;
+  }
+  
 </style>
