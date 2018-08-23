cd47f0417fcf5cec16e6a3fdb7f41cd12842df12
Jack <ajackofsometrades@gmail.com>
2018-08-22 20:02:15 -0700

    made room for longer text on select

--- src/components/IpsumList.vue
+++ src/components/IpsumList.vue
@@ -30,7 +30,7 @@ export default {
 
 ul {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-gap: 10px;
   margin: 20px;
   
