1c1f4f8d1b74ec63906e7475b9f5daf4e44c0f27
Jack <ajackofsometrades@gmail.com>
2018-08-22 17:43:37 -0700

    filling out detail section

--- src/components/IpsumDetail.vue
+++ src/components/IpsumDetail.vue
@@ -1,11 +1,18 @@
 <template>
-  
+  <section>
+    <h3>{{ ipsum.name }}</h3>
+    <p>{{ ipsum.category }}</p>
+    <p v-html="ipsum.body"></p>
+  </section>
 </template>
 
 <script>
 export default {
-
+  props: {
+    ipsum: Object
+  }
 };
+
 </script>
 
 <style scoped>
